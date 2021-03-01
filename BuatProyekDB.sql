28407262f88767293ad570ff458f5f100ae62bdb
Devi Rukmana B <67756819+devirukmanab@users.noreply.github.com>
2021-03-01 16:49:35 +0700

    Update BuatProyekDB.sql

--- BuatProyekDB.sql
+++ BuatProyekDB.sql
@@ -1,1 +1,18 @@
-CREATE DATABASE book1;
+CREATE DATABASE IF NOT EXISTS book1;
+
+USE book1;
+
+CREATE TABLE IF NOT EXISTS sheet1 (
+	saklar_lampu 	VARCHAR(3)
+);
+
+INSERT INTO sheet1 VALUES
+	('ON');
+
+INSERT INTO sheet1 VALUES
+	('OFF');
+
+SELECT 	saklar_lampu, 
+		COUNT(saklar_lampu)
+FROM 	sheet1
+ORDER BY 	saklar_lampu ASC;
