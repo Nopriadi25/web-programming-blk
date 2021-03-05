17132fa16e1600f0370a9e718268d6e83bc6a897
Devi Rukmana B <67756819+devirukmanab@users.noreply.github.com>
2021-03-05 16:37:57 +0700

    Update latihan_database5.sql

--- latihan/latihan_database5.sql
+++ latihan/latihan_database5.sql
@@ -75,3 +75,29 @@ CREATE VIEW vw_data_pegawai AS
 			No_urut
 	FROM vw_no_urut;
 
+					   
+-- Membuat Kakulator dengan RETURN dan RETURNS function di MYSQL
+DELIMITER ;;
+CREATE FUNCTION sp_kalkulator (
+	bilangan_A 	SMALLINT,
+	operator_bilangan	VARCHAR(6),
+	bilangan_B	SMALLINT
+)	RETURNS	SMALLINT DETERMINISTIC
+
+BEGIN
+	RETURN
+	CASE operator_bilangan
+		WHEN 'tambah' THEN bilangan_A + bilangan_B
+		WHEN 'kurang' THEN bilangan_A - bilangan_B
+		WHEN 'kali' THEN bilangan_A * bilangan_B
+		ELSE bilangan_A / bilangan_B
+	END
+END;;
+DELIMITER ;
+					   
+SELECT sp_kalkulator(10, 'tambah', 5);
+SELECT sp_kalkulator(10, 'kurang', 5);
+SELECT sp_kalkulator(10, 'kali', 5);
+SELECT sp_kalkulator(10, 'bagi', 5);
+					   
+					   
