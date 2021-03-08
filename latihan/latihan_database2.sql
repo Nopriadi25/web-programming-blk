52a4f3f0b45b5c4e7baf9278080f5fa998d7aa42
Devi Rukmana B <67756819+devirukmanab@users.noreply.github.com>
2021-03-08 08:56:52 +0700

    Update revisi latihan_database2.sql

--- latihan/latihan_database2.sql
+++ latihan/latihan_database2.sql
@@ -1,10 +1,11 @@
--- TABEL LAMPU LALU LINTAS
+-- Membuat Tabel Lampu Lalu Lintas
 CREATE TABLE IF NOT EXISTS sheet2(
 	lampu_lalu_lintas VARCHAR(6) PRIMARY KEY
 );
 
-INSERT INTO sheet2 (lampu_lalu_lintas) 
-VALUES ("MERAH"), ("KUNING"), ("HIJAU");
+-- Memasukkan values ke dalam tabel sheet 2
+-- INSERT INTO sheet2 (lampu_lalu_lintas) 
+-- VALUES ("MERAH"), ("KUNING"), ("HIJAU");
 
 SELECT lampu_lalu_lintas AS 'Lampu Lalu Lintas',
 	IF(lampu_lalu_lintas="MERAH",
