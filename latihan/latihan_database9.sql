8e1d5cc800ac4c160553e784940eea308f9deff9
Devi Rukmana B <67756819+devirukmanab@users.noreply.github.com>
2021-03-10 09:36:17 +0700

    Menambahkan Kode pada tabel pegawai
    Alter Table pegawai lalu add column, add foreign key, call dan selesai

--- latihan/latihan_database9.sql
+++ latihan/latihan_database9.sql
@@ -16,9 +16,10 @@ CREATE TABLE IF NOT EXISTS agama(
 -- DROP TABLE agama;
 
 -- -- Menghapus Primary Key
-DELETE FROM agama;
+-- DELETE FROM agama;
 -- DELETE FROM pegawai;
--- Memasukkan values atau nilai ke dalam table
+
+-- Memasukkan values atau nilai ke dalam table agama
 -- INSERT INTO agama values
 -- (1, 'Islam'),
 -- (2, 'Katolik'),
@@ -27,7 +28,13 @@ DELETE FROM agama;
 -- (5, 'Buddha'),
 -- (6, 'Kepercayaan');
 
-
+-- Memasukkan values atau nilai ke dalam table pegawai
+-- INSERT INTO pegawai VALUES
+-- ('197209172005011002','Willy P. Johansyah'),
+-- ('198201312010052001','Devi R. Bidari'),
+-- ('200901202015071004','M.N. Dzakiy'),
+-- ('201507142019031002','M.N Dzaka'),
+-- ('201411142020102003','Erin Eftiana');
 
 -- DROP PROCEDURE sp_agama_pegawai;
 
@@ -57,53 +64,101 @@ DELETE FROM agama;
 --  -- DROP PROCEDURE sp_pegawai_agama
 
 -- -- Membuat Prosedur dari tabel gabungan Pegawai dengan Agama
-DELIMITER ;;
+-- DELIMITER ;;
 
-CREATE PROCEDURE IF NOT EXISTS sp_pegawai_agama(
-	nip_p	VARCHAR(18),
-	kode_p SMALLINT
-)
+-- CREATE PROCEDURE IF NOT EXISTS sp_pegawai_agama(
+-- 	nip_p	VARCHAR(18),
+-- 	kode_p SMALLINT
+-- )
 
-BEGIN
-	UPDATE pegawai SET kode = kode_p
-	WHERE nip_pegawai = nip_p;
+-- BEGIN
+-- 	UPDATE pegawai SET kode = kode_p
+-- 	WHERE nip_pegawai = nip_p;
 
-	SELECT 	pegawai.nip_pegawai,
-			pegawai.nama_pegawai,
-			agama.nama
-	FROM pegawai JOIN agama
-	ON pegawai.kode_agama = agama.kode;
+-- 	SELECT 	pegawai.nip_pegawai,
+-- 			pegawai.nama_pegawai,
+-- 			agama.nama
+-- 	FROM pegawai JOIN agama
+-- 	ON pegawai.kode_agama = agama.kode;
 
-END;;
-DELIMITER ;
+-- END;;
+-- DELIMITER ;
 
 -- Memanggil fungsi sp_agama_pegawai dengan kode dan nama agama
-CALL sp_agama_pegawai('197209172005011002', 1);
+-- CALL sp_agama_pegawai('197209172005011002', 1);
 -- CALL sp_agama_pegawai('198201312010052001', 2);
 -- CALL sp_agama_pegawai('200901202015071004', 3);
 -- CALL sp_agama_pegawai('201507142019031002', 4); 
 -- CALL sp_agama_pegawai('201411142020102003', 5);
 
 -- Membuat Prosedur list agama
+-- DELIMITER ;;
+
+-- CREATE PROCEDURE list_agama(
+-- 	kode_list_agama INT(1)
+-- )
+
+-- BEGIN
+-- 	DECLARE i INT;
+-- 	SET i = 1;
+-- 	WHILE i <= kode_list_agama DO
+-- 		INSERT INTO agama VALUES(
+-- 			CONVERT(i, CHAR),
+-- 			elt(i, 'Islam', 'Katolik', 'Protestan', 'Hindu', 'Buddha', 'Kepercayaan')
+-- 		);
+-- 		SET i = i + 1;
+-- 	END WHILE;
+-- 	SELECT *
+-- 	FROM agama;
+-- END;;
+-- DELIMITER ;
+
+-- Menghapus Tabel Pegawai dan kemudian ditambah values baru di Tabel Pegawai
+-- ALTER TABLE pegawai
+-- 	DROP agama;
+
+ALTER TABLE	 pegawai
+	-- ADD COLUMN kode SMALLINT;
+	-- DROP COLUMN kode;
+
+-- ALTER TABLE pegawai
+	-- ADD	CONSTRAINT fk_pegawai_2_agama
+	-- 	FOREIGN KEY (kode)
+	-- 	REFERENCES agama(kode);
+
 DELIMITER ;;
 
-CREATE PROCEDURE list_agama(
-	kode_list_agama INT(1)
+CREATE PROCEDURE IF NOT EXISTS sp_update_agama2(
+	m_kode_agama VARCHAR(1),
+	m_nip_pgw VARCHAR(18)
 )
 
 BEGIN
-	DECLARE i INT;
-	SET i = 1;
-	WHILE i <= kode_list_agama DO
-		INSERT INTO agama VALUES(
-			CONVERT(i, CHAR),
-			elt(i, 'Islam', 'Katolik', 'Protestan', 'Hindu', 'Buddha', 'Kepercayaan')
-		);
-		SET i = i + 1;
-	END WHILE;
+	UPDATE pegawai
+	SET kode = m_kode_agama
+	WHERE nip_pegawai = m_nip_pgw;
 	SELECT *
-	FROM agama;
+	FROM pegawai;
 END;;
 DELIMITER ;
 
-ALTER 
+-- Memanggil fungsi update agama
+CALL sp_update_agama2('1', '197209172005011002');
+CALL sp_update_agama2('2', '198201312010052001');
+CALL sp_update_agama2('3', '200901202015071004');
+CALL sp_update_agama2('4', '201507142019031002');
+CALL sp_update_agama2('5', '201411142020102003');
+
+-- Membuat View Tabel Update Agama
+-- CREATE VIEW vw_update_agama AS
+-- 	SELECT 	nip_pegawai AS nip,
+-- 			nama_pegawai AS nama,
+-- 			nama_agama AS agama
+-- 	FROM	pegawai, agama
+-- 	WHERE pegawai.kode_agama = agama.kode_agama;
+
+	-- SELECT 	nip,
+	-- 		nama,
+	-- 		list_agama
+	-- FROM pegawai, agama
+	-- WHERE pegawai.kode_agama = agama.kode_agama;		
