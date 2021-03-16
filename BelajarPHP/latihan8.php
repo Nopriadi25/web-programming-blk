28aaf938d46c27c6a77ff63c48081dbdc0b82adc
Devi Rukmana B <67756819+devirukmanab@users.noreply.github.com>
2021-03-16 16:44:48 +0700

    Create latihan8.php

---
+++ BelajarPHP/latihan8.php
@@ -0,0 +1,8 @@
+<?php
+    // Array
+    $nama = array("Abdul", "Budi", "Tri", "Aulia", "Firman");
+    $tmptlahir = array("Jakarta", "Jawa Timur", "Jawa Tengah", "Aceh", "Makasar");
+
+    // Output
+    echo "$nama[0] lahir di $tmptlahir[0]";
+?>
