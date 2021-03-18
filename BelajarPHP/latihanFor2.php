f8c74dbefde423b5ecb5999423a4493201afd28d
Devi Rukmana B <67756819+devirukmanab@users.noreply.github.com>
2021-03-18 11:20:30 +0700

    Membuat Piramid dgn HTML

---
+++ BelajarPHP/latihanFor2.php
@@ -0,0 +1,53 @@
+<!-- Create Stars Pattern Looping FOR -->
+
+<?php
+// Membuat Piramid Bintang dari 1 ke 5
+  for($i = 1; $i <= 5; $i++){
+  echo  str_repeat('*', $i),
+  "</br>";
+}
+
+  echo "<hr>";
+
+// Membuat Piramid Bintang dari 5 ke 1 (Kebalikkannya)  
+for($i = 5; $i >= 1; $i--){
+    echo  str_repeat('*', $i),
+    "</br>";
+}
+
+    echo "<hr>";
+    
+// Membuat Piramid angka 1122334455
+for($i = 0; $i <= 5; $i++){  
+    for($j = 1; $j <= $i; $j++){  
+    echo $i;  
+    }  
+    echo "<br>";  
+}   
+    
+    echo "<hr>";
+
+// Contoh Piramid 12345
+$k = 1;  
+
+for($i = 0; $i < 4; $i++){  
+for($j = 0; $j <= $i; $j++){  
+    echo $k." ";  
+    $k++;  
+    }  
+
+    echo "<br>";  
+}   echo "<hr>";
+
+// Membuat Piramid angka dengan sisipkan 3x3
+// for($i = 0; $i <= 5; $i++){  
+    // for($j = 1; $j <= $i; $j++){  
+    //     echo $i;  
+    // }  
+    // echo "<br>";  
+    // }   
+
+
+
+?>
+
