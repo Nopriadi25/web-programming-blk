af03911631b0ef85a5a1f089aedffdc7577ece86
Devi Rukmana B <67756819+devirukmanab@users.noreply.github.com>
2021-03-18 11:49:38 +0700

    Update latihanFor2.php

--- BelajarPHP/latihanFor2.php
+++ BelajarPHP/latihanFor2.php
@@ -40,14 +40,18 @@ for($j = 0; $j <= $i; $j++){
 }   echo "<hr>";
 
 // Membuat Piramid angka dengan sisipkan 3x3
-// for($i = 0; $i <= 5; $i++){  
-    // for($j = 1; $j <= $i; $j++){  
-    //     echo $i;  
-    // }  
-    // echo "<br>";  
-    // }   
-
-
+for($i = 1; $i <= 5; $i++){  
+    for($j = $i; $j >=1; $j--){  
+       
+      $a = $i;
+      $b = $j; 
+        if($a == 3 && $b == 2){
+            echo "X";
+            $j--;
+       }  
+       echo $i;
+     } 
+     echo "</br>";
+ } echo "</hr>";
 
 ?>
-
