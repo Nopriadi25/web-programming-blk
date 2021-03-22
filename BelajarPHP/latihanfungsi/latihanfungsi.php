05b2c2acbbcb080526f22598555d0dbb667435e1
Devi Rukmana B <67756819+devirukmanab@users.noreply.github.com>
2021-03-22 11:04:35 +0700

    Update latihanfungsi.php

--- BelajarPHP/latihanfungsi/latihanfungsi.php
+++ BelajarPHP/latihanfungsi/latihanfungsi.php
@@ -6,18 +6,11 @@
         return $luas;
     }
 
-    echo "Luas segitiga dengan alas 7 dan tinggi 15 = ".luas_segitiga(7, 15);
-    echo "<br>";
-
     // Menghitung Rumus Luas Lingkaran
     function luas_lingkaran($r) {
         $luas = 3.14 * $r * $r;
         return $luas;
     }
-
-    echo "Luas lingkaran dengan jari-jari 20 = ".luas_lingkaran(20);
-    echo "<br>";
-
     
     // Menghitung Rumus Jajaran Genjang
     function luas_jajargenjang($alas, $tinggi) {
@@ -25,19 +18,12 @@
         return $luas;
     }
 
-    echo "Luas jajaran genjang dengan alas 7 dan tinggi 20 = ".luas_jajargenjang(7, 20);
-    echo "<br>";
-
-
     // RUMUS FUNGSI KELILING
     // Menghitung Rumus Keliling Segitiga
-    function keliling_segitiga($a, $b, $c) {
-        $keliling = $a + $b + $c;
+    function keliling_segitiga($SisiA, $SisiB, $SisiC) {
+        $keliling = $SisiA +  $SisiB + $SisiC;
         return $keliling;
     }
-
-    echo "Jumlah keliling segitiga dengan a=5, b=8, dan c=7 = ".keliling_segitiga(5, 8, 7);
-    echo "<br>";
 
     // Menghitung Rumus Keliling Lingkaran
     function keliling_lingkaran($r) {
@@ -45,17 +31,9 @@
         return $keliling;
     }
 
-    echo "Jumlah keliling lingkaran dengan jari-jari 20 = ".keliling_lingkaran(20);
-    echo "<br>";
-
     // Menghitung Rumus Keliling Jajaran Genjang
-    function keliling_jajargenjang($a, $b) {
-        $keliling = 2 * $a + $b;
+    function keliling_jajargenjang($SisiA, $SisiB) {
+        $keliling = 2 * $SisiA + $SisiB;
         return $keliling;
     }
-
-    echo "Jumlah keliling jajaran genjang dengan a=5 dan b=8  = ".keliling_jajargenjang(5, 8);
-    echo "<br>";
-
-
 ?>
